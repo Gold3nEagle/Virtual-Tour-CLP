666159db83eaa8827afacec546a85d185cb5b7f1
TeryakiiSauce <48337702+TeryakiiSauce@users.noreply.github.com>
2023-03-05 17:57:27 +0300

    added smooth rotation to the player

--- Assets/Input System/Controller.cs
+++ Assets/Input System/Controller.cs
@@ -3,7 +3,8 @@ using UnityEngine.InputSystem;
 
 public class Controller : MonoBehaviour
 {
-    public float walkSpeed = 1f;
+    public Animator playerAnim;
+    public float walkSpeed = 10f;
     public float jumpSpeed = 5f;
     private Rigidbody playerRigidbody;
     private PlayerControls playerControls;
@@ -37,11 +38,38 @@ public class Controller : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Vector2 movementDirection = playerControls.Land.Move.ReadValue<Vector2>();
         Debug.Log("Moving... -> Direction: " + movementDirection);
         playerRigidbody.AddForce(new Vector3(movementDirection.x, 0, movementDirection.y) * walkSpeed, ForceMode.Force);
+
+        if (movementDirection == Vector2.zero)
+        {
+            playerAnim.SetTrigger("idle");
+            playerAnim.ResetTrigger("walk");
+        }
+        else
+        {
+            playerAnim.SetTrigger("walk");
+            playerAnim.ResetTrigger("idle");
+
+            float _targetRotation = Mathf.Atan2(movementDirection.x, movementDirection.y) * Mathf.Rad2Deg;
+            float _rotationVelocity = 20f;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, 0.07f);
+
+            // rotate to face input direction relative to camera position
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        if (movementDirection == Vector2.left)
+        {
+            Debug.Log("Turning left...");
+        }
+        else if (movementDirection == Vector2.right)
+        {
+            Debug.Log("Turning right...");
+        }
     }
 
     // Player movements
