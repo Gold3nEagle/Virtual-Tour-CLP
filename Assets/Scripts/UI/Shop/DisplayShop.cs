03d6b9b3b486175205fba040b05b1d3e7ee63d19
Sayed Mohamed Naser <201800276@student.polytechnic.bh>
2023-05-04 14:18:51 +0300

    fixed shop prompt

--- Assets/Scripts/UI/Shop/DisplayShop.cs
+++ Assets/Scripts/UI/Shop/DisplayShop.cs
@@ -6,13 +6,15 @@ public class DisplayShop : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("im here in the shop " + other.name);
-        GameManager.instance.isNearShop = true;
-        GameManager.instance.ShowPrompt("Press F to open the shop");
+        if(other.tag == "Player")
+        {
+            GameManager.instance.isNearShop = true;
+            GameManager.instance.ShowPrompt("Press F to open the shop");
+        } 
     }
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("im not in the shop " + other.name);
+        
         GameManager.instance.isNearShop = false;
         GameManager.instance.HidePrompt();
     }
